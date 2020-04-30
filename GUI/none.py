0f96d15051bafaacb0f90b0f3a7f6f1278d40a21
ayieko168 <antonyalen1960@gmail.com>
2020-04-30 09:22:00 -0400

    commit

--- GUI/none.py
+++ GUI/none.py
@@ -1,4 +1,17 @@
 from requests_html import HTMLSession
+import urllib.parse
 
-url = "http://192.168.0.17:8000/Arduino%20Projects%20Research/"
+url = "http://192.168.0.17:8000/Arduino%20Projects%20Research/Arduino%20Oscciloscope/"
+
+print(f"Starting script, Visiting site...")
+session = HTMLSession()
+r = session.get(url)
+
+for page in r.html:
+    for li_element in page.find("ul li"):
+        print(li_element.absolute_links)
+
+
+
+
 
